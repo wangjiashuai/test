f1a87622f80955204ff461feae26bf32470239da
wangjiashuai <wangjiashuai@wangjiashuaitekiMac-mini.local>
2013-11-08 09:45:21 +0800

    开游戏主界面UI

---
+++ English/projects/English/Classes/GameMainScene.cpp
@@ -0,0 +1,9 @@
+//
+//  GameMainScene.cpp
+//  English
+//
+//  Created by wangjiashuai on 8/11/13.
+//
+//
+
+#include "GameMainScene.h"
---
+++ English/projects/English/Classes/GameMainScene.h
@@ -0,0 +1,20 @@
+//
+//  GameMainScene.h
+//  English
+//
+//  Created by wangjiashuai on 8/11/13.
+//
+//
+
+#ifndef __English__GameMainScene__
+#define __English__GameMainScene__
+#include "cocos2d.h"
+USING_NS_CC;
+
+class GameMainScene : public CCLayer {
+public:
+    CREATE_FUNC(GameMainScene);
+};
+
+
+#endif /* defined(__English__GameMainScene__) */
